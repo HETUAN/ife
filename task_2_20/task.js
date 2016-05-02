d0e279f3a2968f64abca9bbd975ebfb7000ccd3e
HETUAN <hpwtuan@foxmail.com>
2016-05-02 23:50:00 +0800

    sdaf

--- task_2_20/task.js
+++ task_2_20/task.js
@@ -1,1 +1,11 @@
-task.js
+var init = function() {
+    var insertbtn = document.getElementById("insertbtn");
+    insertbtn.addEventListener("click", function() {
+        //
+        var inpttext = document.getElementById("inpttext");
+        var texts = inpttext.split("");
+        for(var key in texts)
+        { 
+        }
+    })
+}
